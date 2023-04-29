5814cde1b53393423dd3ed5f1a2d184158f77009
JarolSmith <u20201b559@upc.edu.pe>
2023-04-28 20:16:51 -0500

    Se agrego un required para validar los campos de entrada al momento de registrar datos

--- data.js
+++ data.js
@@ -23,86 +23,87 @@ module.exports = function () {
         id:1,
         aulaDestino: "201",
         pabellonDestino: "A",
-        facultadDestino: "abc",
+        facultadDestino: "Facultad de Ciencias",
       },
       {
         id:2,
         aulaDestino: "301",
         pabellonDestino: "B",
-        facultadDestino: "bcd",
+        facultadDestino: "Facultad de ingenieria",
       },
       {
         id:3,
         aulaDestino: "401",
         pabellonDestino: "C",
-        facultadDestino: "cde",
+        facultadDestino: "Facultad de derechos",
       },
       {
         id:4,
         aulaDestino: "501",
         pabellonDestino: "D",
-        facultadDestino: "def",
+        facultadDestino: "Facultad de Ciencias Sociales",
       }
     ],
     membresias:[
       {
         id:1,
-        descripcionTipoDeMembresia: "aaa",
+        descripcionTipoDeMembresia: "Premium",
         fechainicioTipoDeMembresia: "2022-03-10",
         fechafinTipoDeMembresia: "2023-03-15",
       },
       {
         id:2,
-        descripcionTipoDeMembresia: "bbb",
+        descripcionTipoDeMembresia: "platino",
         fechainicioTipoDeMembresia: "2022-03-11",
         fechafinTipoDeMembresia: "2023-03-15",
       },
       {
         id:3,
-        descripcionTipoDeMembresia: "ccc",
+        descripcionTipoDeMembresia: "Freemium",
         fechainicioTipoDeMembresia: "2022-03-12",
         fechafinTipoDeMembresia: "2023-03-15",
       },
       {
         id:4,
-        descripcionTipoDeMembresia: "ddd",
+        descripcionTipoDeMembresia: "Premium",
         fechainicioTipoDeMembresia: "2022-03-05",
         fechafinTipoDeMembresia: "2023-03-15",
       }
     ],
     universidades:[
       {
-        id:1,
-        nombreUniversidad: "aaa",
-        regionUniversidad: "aaa",
-        distritoUniversidad: "aaa",
-        calleUniversidad:"aaa",
-        telefonoUniversidad:"aaa",
+        id: 1,
+        nombreUniversidad: "Universidad Nacional de Ingeniería",
+        regionUniversidad: "Lima",
+        distritoUniversidad: "Rímac",
+        calleUniversidad: "Av. Túpac Amaru 210",
+        telefonoUniversidad: "(01) 4811070"
       },
       {
-        id:2,
-        nombreUniversidad: "bbb",
-        regionUniversidad: "bbb",
-        distritoUniversidad: "bbb",
-        calleUniversidad:"bbb",
-        telefonoUniversidad:"bbb",
+        id: 2,
+        nombreUniversidad: "Universidad del Pacífico",
+        regionUniversidad: "Lima",
+        distritoUniversidad: "Magdalena del Mar",
+        calleUniversidad: "Av. Salaverry 2020",
+        telefonoUniversidad: "(01) 2190100"
       },
       {
-        id:3,
-        nombreUniversidad: "ccc",
-        regionUniversidad: "ccc",
-        distritoUniversidad: "ccc",
-        calleUniversidad:"ccc",
-        telefonoUniversidad:"ccc",
+        id: 3,
+        nombreUniversidad: "Universidad Nacional de Trujillo",
+        regionUniversidad: "La Libertad",
+        distritoUniversidad: "Trujillo",
+        calleUniversidad: "Av. Juan Pablo II s/n",
+        telefonoUniversidad: "(044) 233678"
       },
       {
-        id:4,
-        nombreUniversidad: "ddd",
-        regionUniversidad: "ddd",
-        distritoUniversidad: "ddd",
-        calleUniversidad:"ddd",
-        telefonoUniversidad:"ddd",
+        id: 4,
+        nombreUniversidad: "Universidad Nacional San Antonio Abad del Cusco",
+        regionUniversidad: "Cusco",
+        distritoUniversidad: "Cusco",
+        calleUniversidad: "Av. de la Cultura 733",
+        telefonoUniversidad: "(084) 237890"
       }
+      
     ]
   }
   return data
